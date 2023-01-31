d54bf905a8decef129f2528e0528c8cfd720ca35
Админ <@DESKTOP-H16T94L>
2023-01-31 12:04:06 +0300

    Добавьте файлы проекта.

---
+++ City.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp1
+{
+    internal class City
+    {
+        public string Title { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Time { get; set; }
+        public double Temperature { get; set; }
+        public double Windspeed { get; set; }
+    }
+}
---
+++ CityViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WeatherApp1
+{
+    internal class CityViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangingEventHandler PropertyChanged;
+        public City city;
+        public ICommand LoadDataCommand { protected set; get; }
+        public CityViewModel()
+    }
+}
